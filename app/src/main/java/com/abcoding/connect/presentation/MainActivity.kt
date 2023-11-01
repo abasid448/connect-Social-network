7a2fa512f1e2ed71c6ecd1b890c4f38a53731bb0
abasid448 <abasid448@gmail.com>
2023-11-01 16:44:53 +0530

    first Commit

---
+++ app/src/main/java/com/abcoding/connect/presentation/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.abcoding.connect.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.abcoding.connect.presentation.ui.theme.ConnectTheme
+import com.abcoding.connect.presentation.util.Navigation
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ConnectTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Navigation()
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/abcoding/connect/presentation/util/Navigation.kt
@@ -0,0 +1,25 @@
+package com.abcoding.connect.presentation.util
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.abcoding.connect.presentation.login.LoginScreen
+import com.abcoding.connect.presentation.splash.SplashScreen
+
+@Composable
+fun Navigation(){
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = Screen.SplashScreen.route
+    ){
+         composable(Screen.SplashScreen.route){
+             SplashScreen(navController = navController )
+         }
+        composable(Screen.LoginScreen.route){
+                LoginScreen(navController = navController)
+         }
+    }
+}
